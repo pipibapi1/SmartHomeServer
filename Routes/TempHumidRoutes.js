44b78b0a69ac47250f4649c208ee63f2e2660916
pipibapi1 <pipibapi1@gmail.com>
2022-03-25 10:09:19 +0700

    add temperature and humid server route

---
+++ Routes/TempHumidRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("GET TEMPERATURE AND HUMIDITY INFORMATION");
+});
+
+export default router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import accountRoute from "./Routes/AccountRoutes.js";
 import userInfoRoute from "./Routes/UserInfoRoutes.js";
+import tempHumidRoute from "./Routes/TempHumidRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 });
 app.use("/account", accountRoute);
 app.use("/userinfo", userInfoRoute);
+app.use("/temphumid", tempHumidRoute);
 
 app.listen(port, () =>
   console.log(`Server is Running on port: http://localhost:${port}`)
