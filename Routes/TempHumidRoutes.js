f31ee88e14bd4dc956679f9947762f93dae52096
pipibapi1 <pipibapi1@gmail.com>
2022-03-25 10:13:12 +0700

    Silly update

--- Routes/TempHumidRoutes.js
+++ Routes/TempHumidRoutes.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("GET TEMPERATURE AND HUMIDITY INFORMATION");
+  res.send("GET TEMPERATURE AND HUMIDITY INFORMATION!!!!!!!");
 });
 
 export default router;
