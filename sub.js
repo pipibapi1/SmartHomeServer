c7bae428562963d7b9bae148f7a48236f8eae9bb
pipibapi1 <pipibapi1@gmail.com>
2022-03-26 14:26:05 +0700

    remove sub file

--- sub.js
+++
@@ -1,9 +0,0 @@
-var mqtt = require("mqtt");
-var client = mqtt.connect("mqtt://test.mosquitto.org", {
-  clientId: "mqttjs01",
-});
-console.log(client);
-client.on("connect", function () {
-  console.log("connected");
-});
-client.end();
