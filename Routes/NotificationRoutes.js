9b3a106e0903fdd3095eab70e4a48bbb6daa7f30
pipibapi1 <pipibapi1@gmail.com>
2022-04-06 15:39:26 +0700

    update notification router

--- Routes/NotificationRoutes.js
+++ Routes/NotificationRoutes.js
@@ -3,7 +3,7 @@ const Notification = require("../Models/NotificationModel");
 
 const router = express.Router();
 
-router.get("/get-all", (req, res) => {
+router.get("/get-all", async (req, res) => {
   Notification.find(function (err, accounts) {
     if (err) {
       console.log(err);
@@ -13,6 +13,14 @@ router.get("/get-all", (req, res) => {
   });
 });
 
-router.get("/post-notification", (req, res) => {});
+router.get("/post-notification", async (req, res) => {
+  const { type, content, date } = req.body;
+  const newNotification = new Notification({
+    type: type,
+    content: content,
+    date: date,
+  });
+  newNotification.save();
+});
 
 module.exports = router;
