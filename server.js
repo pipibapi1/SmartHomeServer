7a613114637f9b54a70a907eb8f9be851434afd1
pipibapi1 <pipibapi1@gmail.com>
2022-04-06 10:16:22 +0700

    add cor header

--- server.js
+++ server.js
@@ -86,7 +86,33 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("THIS IS THE CAFE COT DUA SMART HOME SERVER");
 });
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://smart-home-server-cafecotdua.herokuapp.com"
+  );
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use("/account", accountRoute);
 app.use("/userinfo", userInfoRoute);
 app.use("/temphumid", tempHumidRoute);
