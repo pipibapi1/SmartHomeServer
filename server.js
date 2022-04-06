e95b7996909c022550586fe9ab3d646855050059
pipibapi1 <pipibapi1@gmail.com>
2022-04-06 15:49:30 +0700

    update CORS header

--- server.js
+++ server.js
@@ -90,15 +90,23 @@ app.get("/", (req, res) => {
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
-
+  req.setHeader("Access-Control-Allow-Origin", "*");
   // Request methods you wish to allow
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-
+  req.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
   // Request headers you wish to allow
   res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  req.setHeader(
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
@@ -106,7 +114,7 @@ app.use(function (req, res, next) {
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
-
+  req.setHeader("Access-Control-Allow-Credentials", true);
   // Pass to next layer of middleware
   next();
 });
