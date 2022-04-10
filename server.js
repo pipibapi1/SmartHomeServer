481d91dcf48e27053149f2d3b2c3934adcc851fa
pipibapi1 <pipibapi1@gmail.com>
2022-04-10 10:20:09 +0700

    add control light actitvity notification

--- server.js
+++ server.js
@@ -146,13 +146,9 @@ app.get("/", (req, res) => {
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader("Access-Control-Allow-Origin", "*");
-  req.setHeader("Access-Control-Allow-Origin", "*");
+
   // Request methods you wish to allow
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  req.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
@@ -162,15 +158,11 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  req.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader("Access-Control-Allow-Credentials", true);
-  req.setHeader("Access-Control-Allow-Credentials", true);
+
   // Pass to next layer of middleware
   next();
 });
