21d6807c800f3d924b082cfe3921af8aef10e2ec
ntdbng <ntdb.542001@gmail.com>
2022-04-18 00:36:44 +0700

    Update Login, Signup, ForgotPassword

--- Routes/AccountRoutes.js
+++ Routes/AccountRoutes.js
@@ -29,4 +29,54 @@ router.post("/login", (req, res) => {
   });
 });
 
+router.post("/register", (req, res) => {
+  // console.log(req.body);
+  const find = { userId: req.body.email, email: req.body.email, password: req.body.password, name: req.body.name};
+  console.log(find);
+
+  Account.create(find, function (err, result) {
+    if (err) throw err;
+    else {
+      if (result != null) {
+        // console.log(result.email);
+        res.send("Successfull");
+      }
+    }
+  });
+});
+
+
+router.post("/forgotpassword", (req, res) => {
+  // console.log(req.body);
+  const filter = { email: req.body.email };
+  const update = {
+      password: req.body.password,
+  };
+
+  Account.findOneAndUpdate(filter, update, function (err, result) {
+    if (err) throw err;
+    else {
+      if (result != null) {
+        // console.log(result.email);
+        res.send("Successfull");
+      }
+    }
+  });
+});
+
 module.exports = router;
+  // UserInfo.findOneAndUpdate(filter, update, function (err, res) {
+  //   if (err) throw err;
+  //   console.log("1 document updated");
+  // });
+
+  // const filter = { email: req.body.email };
+  // const update = {
+  //   $set: {
+  //     firstname: req.body.firstname,
+  //     lastname: req.body.lastname,
+  //     phone: req.body.phone,
+  //     gender: req.body.gender,
+  //     birthday: req.body.birthday,
+  //   },
+  // };
